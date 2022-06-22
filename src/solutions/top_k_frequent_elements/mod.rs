984ac72c7d3c8302db315b3255010ea7e1b544ef
Jacob <bayswaterpc@gmail.com>
2022-06-22 19:42:31 -0400

    chore: cargo format

--- src/solutions/top_k_frequent_elements/mod.rs
+++ src/solutions/top_k_frequent_elements/mod.rs
@@ -1,28 +1,25 @@
 use super::Solution;
-use std::collections::HashMap;
 use std::collections::BinaryHeap;
+use std::collections::HashMap;
 impl Solution {
     /// Solution to [Top K Frequent Elements](https://leetcode.com/problems/top-k-frequent-elements/)
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        let mut hashmap = HashMap::new(); 
-        for num in nums { 
+        let mut hashmap = HashMap::new();
+        for num in nums {
             *hashmap.entry(num).or_insert(0) += 1;
         }
-        let freq_num: Vec<(i32, i32)> = hashmap
-            .into_iter()
-            .map(|(a,b)| (b,a))
-            .collect();
-        
+        let freq_num: Vec<(i32, i32)> = hashmap.into_iter().map(|(a, b)| (b, a)).collect();
+
         let mut res = Vec::new();
         let mut maxheap: BinaryHeap<(i32, i32)> = BinaryHeap::from(freq_num);
-         
-        while let Some((_, num)) = maxheap.pop() { 
+
+        while let Some((_, num)) = maxheap.pop() {
             res.push(num);
-            if res.len() == k as usize { 
-                break; 
-            } 
+            if res.len() == k as usize {
+                break;
+            }
         }
-        res        
+        res
     }
 }
 
@@ -32,13 +29,12 @@ mod test {
 
     #[test]
     fn unit() {
-        let nums = vec![1,1,1,2,2,3];
+        let nums = vec![1, 1, 1, 2, 2, 3];
         let k = 2;
-        assert_eq!(vec![1,2], Solution::top_k_frequent(nums, k));
+        assert_eq!(vec![1, 2], Solution::top_k_frequent(nums, k));
 
         let nums = vec![1];
         let k = 2;
         assert_eq!(vec![1], Solution::top_k_frequent(nums, k));
-
     }
 }
