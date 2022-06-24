849793cd4f8e81d316c9b87513a8a11e724515ee
Jacob <bayswaterpc@gmail.com>
2022-06-24 16:41:53 -0400

    chore: clippy fix

--- src/solutions/three_sum/mod.rs
+++ src/solutions/three_sum/mod.rs
@@ -4,7 +4,7 @@ use super::Solution;
 impl Solution {
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut res = Vec::new();
-        nums.sort();
+        nums.sort_unstable();
 
         for i in 0..nums.len() {
             // Skip repeats to avoid duplicates
