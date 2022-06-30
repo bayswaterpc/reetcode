49d46760c83ca11224d1bf013d1f129d6b2ec57a
Jacob <bayswaterpc@gmail.com>
2022-06-30 16:36:27 -0400

    refactor: go shorten word search implementation

--- src/solutions/word_search/mod.rs
+++ src/solutions/word_search/mod.rs
@@ -3,69 +3,42 @@ impl Solution {
     ///Solution to vec![Word Search](https://leetcode.com/problems/word-search/)
     // pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
     pub fn word_search(board: Vec<Vec<char>>, word: String) -> bool {
-        let mut board = board;
-        fn backtrack(
-            board: &mut Vec<Vec<char>>,
-            combo: &mut String,
-            word: &String,
+        fn dfs(
+            board: &[Vec<char>],
+            word: &[char],
+            visited: &mut [Vec<bool>],
             ii: usize,
             jj: usize,
+            depth: usize,
         ) -> bool {
-            println!("{} {}", combo, word);
-            if *combo == *word {
-                println!("-------------- should return true!!!!!");
-                return true;
-            }
-
-            if board[ii][jj] != word.chars().nth(combo.len()).unwrap() {
-                return false;
-            }
-            let c = board[ii][jj];
-            board[ii][jj] = '#';
-            combo.push(c);
-
-            let mut test_dirs = vec![];
-
-            // East
-            if ii < board.len() - 1 {
-                test_dirs.push((ii+1, jj));
-            }
-            // South
-            if jj < board[0].len() - 1 {
-                test_dirs.push((ii, jj+1));
-            }
-            // West
-            if ii > 0 {
-                test_dirs.push((ii-1, jj));
-
-            }
-            // North
-            if jj > 0 {
-                test_dirs.push((ii, jj-1));
-            }
-
-            for (ni, nj) in test_dirs.iter() {
-                if backtrack(board, combo, word, *ni, *nj) {
-                    println!(" returned true please stop.......");
-                    return true;
-                }
-            }
-            
-            combo.pop();
-            board[ii][jj] = c;
-            false
+            depth == word.len()
+                || (ii < board.len()
+                    && jj < board[0].len()
+                    && !visited[ii][jj]
+                    && word[depth] == board[ii][jj]
+                    && {
+                        visited[ii][jj] = true;
+                        let rez = [0, 1, 0, !0, 0].windows(2).any(|w| {
+                            dfs(
+                                board,
+                                word,
+                                visited,
+                                ii.wrapping_add(w[0]),
+                                jj.wrapping_add(w[1]),
+                                depth + 1,
+                            )
+                        });
+                        visited[ii][jj] = false;
+                        rez
+                    })
         }
 
-        let mut combo = "".to_string();
-        for ii in 0..board.len() {
-            for jj in 0..board[0].len() {
-                if backtrack(&mut board, &mut combo, &word, ii, jj) {
-                    println!(" plz.....");
-                    return true;
-                }
-            }
-        }
-        false
+        let m = board.len();
+        let n = board[0].len();
+        let word = word.chars().collect::<Vec<_>>();
+        let mut visited = vec![vec![false; n]; m];
+
+        (0..m).any(|i| (0..n).any(|j| dfs(&board, &word, &mut visited, i, j, 0)))
     }
 }
 
